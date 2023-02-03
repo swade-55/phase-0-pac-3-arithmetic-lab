52e85472a4149d3385dddfa42a010c78753528e0
SamuelWade <samuelrcwade@gmail.com>
2023-02-03 15:58:39 -0500

    Test Successful

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(x,y) {
+    return x+y;
+}
+
+function subtract(x,y) {
+    return x-y;
+}
+
+function multiply(x,y) {
+    return x*y;
+}
+
+function divide(x,y) {
+    return x/y;
+}
+
+function subtract(b,a) {
+    return b-a;
+}
+
+function increment(n) {
+    return n+=1;
+}
+
+function decrement(n) {
+    return n-=1;
+}
+
+function makeInt(n) {
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
